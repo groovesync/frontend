7269547580c99b52940e44f85581f7bf0096674f
Lucas de Medeiros <lucasmsoares011@gmail.com>
2025-02-06 21:05:07 -0300

    Search Results page amo

---
+++ components/ArtistProfilePicture/ArtistProfilePicture.tsx
@@ -0,0 +1,28 @@
+import { Avatar, Box, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import userProfileData from "../../mockData/userProfileData.json"
+
+const ArtistProfilePicture: React.FC<{name: string, pictureURL: string, pageURL: string}> = ({name, pictureURL, pageURL}) => {
+    return (
+        <Box
+        width="160px"
+        cursor={"pointer"}
+        as={Link}
+        href={pageURL}
+        display="flex"
+        flexDirection={"column"}
+        alignItems="center">
+            <Avatar src={pictureURL} w="160px" h="160px" 
+            _hover={{ boxShadow: "md", transform: "scale(1.05)" }} 
+            transition="all 0.2s ease-in-out" mr={4} />
+
+            <Box p={2} w="100%" textAlign="left">
+                <Text fontSize="16px" fontWeight="medium" color="brand.500" isTruncated>
+                {name}
+                </Text>
+            </Box>
+        </Box>
+    );
+};
+
+export default ArtistProfilePicture;
